6a80930764807bde716ada60408fa272a089688a
aliciabierly <anbierly@gmail.com>
2026-08-06 16:15:51 -0700

    fixed python file and created final matrix for TR and heterozygousity

--- longTR/calculate_tr_repeat_lengths.py
+++ longTR/calculate_tr_repeat_lengths.py
@@ -10,13 +10,11 @@ def calculate_repeat_length(v, keep_all_rows):
     keep_all_rows: string
     This function takes a row of a vcf file (one TR variant)
     and outputs the repeat length for a TR loci per allele
+    and whether the individual is homozygous for the TR.
     '''
-
+    # get variables from line
     period = int(v.INFO.get("PERIOD"))
     ref_length = len(v.REF)
-    #format = v.FORMAT.split(":")
-    #gb_position = format.index("GB")
-    #sample_info = v.SAMPLE.split(":")
     gb = v.format("GB")[0].split("|")
 
     # calculations
@@ -30,22 +28,26 @@ def calculate_repeat_length(v, keep_all_rows):
             repeat_lengths.append("NA")
         else:
             repeat_lengths.append(str(length))
-
-    return f"{repeat_lengths[0]}|{repeat_lengths[1]}"
+    het = repeat_lengths[0] != repeat_lengths[1]
+    return f"{repeat_lengths[0]}|{repeat_lengths[1]}", het
 
 def main():
     input_vcf = VCF(sys.argv[1])
     input_vcf.add_info_to_header({"ID": "R",
                             "Description": "repeat number per allele",
                             "Type": "Character", "Number": "1"})
+    input_vcf.add_info_to_header({"ID": "HET",
+                            "Description": "het=1, homo=0",
+                            "Type": "Integer", "Number": "1"})
 
     output_vcfname = sys.argv[2]
     output_vcf = Writer(output_vcfname, input_vcf)
     keep_all_rows = sys.argv[3]
 
     for v in input_vcf:
-        r = calculate_repeat_length(v, keep_all_rows)
+        r, het = calculate_repeat_length(v, keep_all_rows)
         v.INFO["R"] = r
+        v.INFO["HET"] = het
         output_vcf.write_record(v)
 
     output_vcf.close(), input_vcf.close()
