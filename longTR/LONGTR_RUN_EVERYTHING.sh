907f44a3721bbe7ac51237ab95550fcb6a221f55
aliciabierly <anbierly@gmail.com>
2026-08-10 15:15:19 -0700

    document dependency for sbatch files to run all of longTR pipeline

--- longTR/LONGTR_RUN_EVERYTHING.sh
+++ longTR/LONGTR_RUN_EVERYTHING.sh
@@ -5,8 +5,7 @@ jobid_longtr_full_pipeline=$(sbatch --parable longtr_full_pipeline_copy.sbatch)
 jobid_filter_longtr_by_dp_q=$(sbatch --parable=afterok:$jobid_longtr_full_pipeline filter_longtr_by_dp_q_copy.sbatch)
 # delete intermediate files
 jobid_create_bed_for_matrix=$(sbatch --parable=afterok:$jobid_filter_longtr_by_dp_q create_bedfile_for_matrix.sbatch)
-sbatch 
 
-check_vcf_files_genotyped.sbatch   logs                                       longtr_remove_files.sbatch
-create_bedfile_for_matrix.sbatch   longtr_filter_vcf_find_num_repeats.sbatch  lontr_number_of_repeats_check.py
-filter_longtr_by_dp_q_copy.sbatch  longtr_full_pipeline_copy.sbatch           number_of_repeats_check.sbatch
+jobid_make_matrix=$(sbatch --parable=afterok:$jobid_create_bed_for_matrix make_matrix.sbatch)
+
+jobid_transform_matrix=$(sbatch --parable=afterok:$jobid_make_matrix transform_matrix.sbatch)
