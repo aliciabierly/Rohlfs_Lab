6baf21967bf9e6cbb269dca946c8493c34b20c21
aliciabierly <anbierly@gmail.com>
2026-08-24 11:41:21 -0700

    add line for mean to make comparison between runs easier to see

--- longTR/deletions/deletion_plots.py
+++ longTR/deletions/deletion_plots.py
@@ -9,16 +9,22 @@ df = pd.read_csv(file_path, sep="\t", header=None)
 fig, axes = plt.subplots(1, 2, figsize=(14, 5))
 
 # Number of deletions per sample
-axes[0].hist(df.value_counts(0), bins=np.arange(0, 20000, 500))
+df_sub=df.value_counts(0).reset_index()
+mean=np.mean(df_sub["count"])
+axes[0].hist(df_sub["count"], bins=np.arange(0, 20000, 500))
+axes[0].axvline(mean, label=f"mean: {np.round(mean)}", color="r")
 axes[0].set_xlabel("Number of Deletions")
 axes[0].set_ylabel("Count")
-axes[0].set_title(f"Number of Deletions per Sample (n={df.value_counts(0).reset_index().shape[0]})")
+axes[0].set_title(f"Number of Deletions per Sample (n={df_sub.shape[0]})")
 
 # Frequency of deletions per loci
-axes[1].hist(df.value_counts([1, 2]), bins=np.arange(0, 20))
+df_sub=df.value_counts([1, 2]).reset_index()
+mean=np.mean(df_sub["count"])
+axes[1].hist(df_sub["count"], bins=np.arange(0, 20))
+axes[1].axvline(mean, label=f"mean: {np.round(mean)}", color="r")
 axes[1].set_xlabel("Number of Deletions")
 axes[1].set_ylabel("Count")
-axes[1].set_title(f"Frequency of Deletions per Loci (n={df.value_counts([1, 2]).reset_index().shape[0]})")
+axes[1].set_title(f"Frequency of Deletions per Loci (n={df_sub.shape[0]})")
 
 fig.suptitle(SPECIFIC_DIR)
 plt.tight_layout()
